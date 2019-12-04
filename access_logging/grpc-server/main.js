e30ab03c8edc27e4fa37f4daa13cb57a822c6063
Shekhar Patnaik <patnaikshekhar@gmail.com>
2019-12-04 15:17:47 +0000

    Fixed config file

--- access_logging/grpc-server/main.js
+++ access_logging/grpc-server/main.js
@@ -2,8 +2,8 @@ const grpc = require('grpc')
 const alsMessages = require('./static_codegen/envoy/service/accesslog/v2/als_pb')
 const alsService = require('./static_codegen/envoy/service/accesslog/v2/als_grpc_pb')
 
-
 function streamAccessLogs(call) {
+    console.log('Here')
     call.on('data', (data) => {
         console.log(data)
     })
@@ -13,6 +13,7 @@ function main() {
     const server = new grpc.Server()
     server.addService(alsService.AccessLogServiceService, { streamAccessLogs: streamAccessLogs })
     server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
+    server.start()
 }
 
 main()
